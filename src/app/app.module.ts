1c5be1a90f2ad50e72886e4241dda6d9e7f083ec
akash sanap <akashsanap0000@gmail.com>
2022-04-04 16:58:12 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
